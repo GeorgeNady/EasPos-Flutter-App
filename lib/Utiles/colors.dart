d79200ac36bb39d1fb50b489828737bac71b0950
ibrahimresheh <ibrahim4github@gmail.com>
2021-11-29 08:42:21 +0200

    new

--- lib/Utiles/colors.dart
+++ lib/Utiles/colors.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 abstract class MyColors{
-  static Color scaffoldBackground = Color(0xFF002171);
+  static Color scaffoldBackground = Color(0xFFF5F5F5);
   static Color primaryColor = Color(0xFF111111);
   static Color accentColor = Color(0xFF2196f3);
-  static Color black = Color(0xFF000000);
+
+  static Color black = Color(0xFF4D4F5C);
   static Color white = Color(0xFFFFFFFF);
+  static Color grey = Color(0xFFBCBCCB);
+  static Color red = Color(0xFFD24141);
+  static Color darkRed = Color(0xFFBD3A3A);
+  static Color green = Color(0xFF5EE2A0);
+  static Color purple = Color(0xFFA3A0FB);
+  static Color blue = Color(0xFF55D8FE);
+  static Color yellow = Color(0xFFFFDA83);
+
   static Color cardBackground = Color(0xFF332171);
   static Color textBackground = Color(0xFF4D2171);
   static Color textColor = Color(0xFFFFFFFF);
--- lib/Utiles/constants.dart
+++ lib/Utiles/constants.dart
@@ -1,4 +1,18 @@
 class Constants {
   static const String USERS_COLLECTION = "users";
   static const String REQUESTS_COLLECTION = "requests";
+
+
+  static const Map<String, String> SIDE_MENU_ITEMS = {
+    'الرئيسية': 'home',
+    'الأصناف والفئات': 'products',
+    'العملاء': 'customers',
+    'الفواتير': 'invoices',
+    'المشتريات': 'invoices',
+    'الحسابات': 'money',
+    'التقارير': 'dashboard',
+    'الإعدادات': 'settings',
+    'مركز المساعدة': 'help',
+  };
+
 }
